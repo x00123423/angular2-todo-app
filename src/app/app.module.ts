4e8e030db6e5be4e7aff1ee44845bbd233a25bfb
Enda Lee <enda.lee@ittdublin.ie>
2016-11-21 16:53:35 +0000

    Starting lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { MyNewDirectiveDirective } from './my-new-directive.directive';
+import { MyNewPipePipe } from './my-new-pipe.pipe';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    MyNewDirectiveDirective,
+    MyNewPipePipe,
+    TodoAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-todo-app',
+  templateUrl: 'todo-app.component.html',
+  styleUrls: ['todo-app.component.css']
+})
+export class TodoAppComponent {
+
+  constructor() {}
+
+}
